9a3e1f7b630edfcb8830f0da529de85f867dfdca
Parker Moore <parkrmoore@gmail.com>
2011-01-19 23:35:00 -0500

    Reorganized into folder: bash

---
+++ bash/tutorial/BASHScript.sh
@@ -0,0 +1,12 @@
+#! /bin/bash 
+# (shebang is #!)
+# Mathieu Lavalle-Adam
+# Student I.D.: Unimportant.
+
+# Open a new command line prompt
+mkdir FIRSTSCRIPT
+cd FIRSTSCRIPT
+vi NUMBERS.txt
+sort -bfn NUMBERS.txt > SORTED.txt
+more SORTED.txt
+
