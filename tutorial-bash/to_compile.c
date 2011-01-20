b607e53bbddce7575394c7c13c623efc1efa86c8
Parker Moore <parkrmoore@gmail.com>
2011-01-19 23:12:51 -0500

    initial commit

---
+++ tutorial-bash/to_compile.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	printf("To compile C, type:\n");
+	printf("\tgcc filename.c\n");
+	printf("To run the file you just compiled, type:\n");
+	printf("\t./filename.out\n");
+	printf("\nYou're done.\n");
+}
