b5025d03415d158a9297d0460a2715378177fb79
Parker Moore <parkrmoore@gmail.com>
2011-01-19 23:39:24 -0500

    Reorganized again

--- bash/tutorial/hello_world.c
+++ bash/tutorial/hello_world.c
@@ -1,3 +1,5 @@
+/* This file, though C in the BASH folder, was used to illustrate how we compile C programs in bash, namely the gcc command. */
+
 #include <stdio.h>
 int main(){
 	printf("Hello World\n");
--- tutorial-bash/to_compile.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-	printf("To compile C, type:\n");
-	printf("\tgcc filename.c\n");
-	printf("To run the file you just compiled, type:\n");
-	printf("\t./filename.out\n");
-	printf("\nYou're done.\n");
-}
