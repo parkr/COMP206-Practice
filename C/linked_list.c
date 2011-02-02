c4207fd2009520de235eebbfaf58d1707d114bf6
Parker Moore <parkrmoore@gmail.com>
2011-02-02 00:29:12 -0500

    Fixed linked_list.c thanks to valuable information from Leif

--- C/linked_list.c
+++ C/linked_list.c
@@ -17,11 +17,10 @@ int main(void){ /* Add the following integers into a linked list */
 	scanf("%d", &value);
 	
 	while(value != 0){
-		temp = (struct NODE *) malloc(sizeof(struct NODE));
-		temp.data = value;
+		temp = malloc(sizeof(*temp));
+		temp->data = value;
 		
-		if(head == NULL) temp.next = NULL;
-		else temp.next = head;
+		temp->next = head;
 		
 		head = temp;
 		
@@ -30,8 +29,8 @@ int main(void){ /* Add the following integers into a linked list */
 	}
 	
 	printf("Dump the contents of the linked list:\n");
-	for (temp=head; temp!=NULL; temp=temp.next)
-		printf("Data = %d\n", temp.data);
+	for (temp=head; temp!=NULL; temp=temp->next)
+		printf("Data = %d\n", temp->data);
 	
 	return 0;
 }
